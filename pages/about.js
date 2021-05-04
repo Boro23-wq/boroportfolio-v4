91f5f89f5ffb07a84b1a1e2415a356e792138524
Boro23 <404slimboysavage@gmail.com>
2021-05-04 16:09:53 +0530

    added-pages

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import { Avatar } from '@chakra-ui/react'
+import { ArrowRightIcon } from '@chakra-ui/icons'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
 import { PageSeo } from '@/components/SEO'
+import SpotifyPlayer from '@/components/SpotifyPlayer'
 
 export default function About() {
   return (
@@ -10,45 +13,77 @@ export default function About() {
         description={`About me - ${siteMetadata.author}`}
         url={`${siteMetadata.siteUrl}/about`}
       />
-      <div className="divide-y">
+      <div className="divide-y-2 divide-gray-100 dark:divide-gray-900">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+          <h1 className="text-4xl font-black leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Hey there, I'm {siteMetadata.author.split(' ')[1]}.
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center pt-8 space-x-2">
-            <img src={siteMetadata.image} alt="avatar" className="w-48 h-48 rounded-full" />
+            {/* <Avatar name={siteMetadata.author} src={siteMetadata.image} /> */}
+            <img
+              src={siteMetadata.image}
+              alt={siteMetadata.author}
+              className="w-48 h-48 rounded-full"
+            />
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
               {siteMetadata.author}
             </h3>
-            <div className="text-gray-500 dark:text-gray-400">Professor of Atmospheric Science</div>
-            <div className="text-gray-500 dark:text-gray-400">Stanford University</div>
+            <div className="text-gray-500 dark:text-gray-400 mb-4">
+              Student, developer, and blogger.
+            </div>
+            <SpotifyPlayer />
+            <div className="text-gray-500 dark:text-gray-400"></div>
             <div className="flex pt-6 space-x-3">
               <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
               <SocialIcon kind="github" href={siteMetadata.github} />
-              <SocialIcon kind="facebook" href={siteMetadata.facebook} />
-              <SocialIcon kind="youtube" href={siteMetadata.youtube} />
+              {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} /> */}
+              {/* <SocialIcon kind="youtube" href={siteMetadata.youtube} /> */}
               <SocialIcon kind="linkedin" href={siteMetadata.linkedin} />
-              <SocialIcon kind="twitter" href={siteMetadata.twitter} />
+              {/* <SocialIcon kind="twitter" href={siteMetadata.twitter} /> */}
             </div>
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
-            <p>
-              Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His
-              research interests includes complexity modelling of tailwinds, headwinds and
-              crosswinds.
-            </p>
-            <p>
-              He leads the clean energy group which develops 3D air pollution-climate models, writes
-              differential equation solvers, and manufactures titanium plated air ballons. In his
-              free time he bakes raspberry pi.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique
-              placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem
-              nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
-            </p>
+            <div className="mb-4">
+              <div className="text-3xl tracking-tight font-black mb-3">About</div>
+              <div className="flex mb-2">
+                {/* <ArrowRightIcon w={2} h={2} mt={3} mr={4} /> */}‣ I'm a senior year
+                undergraduate studying Computer Science. I work on building highly performant web
+                applications. I'm predisposed towards design-centric approach, and at the same time
+                learning to devise high-impact user experiences.
+              </div>
+              <div className="flex mb-2">
+                {/* <ArrowRightIcon w={2} h={2} mt={3} mr={4} /> */}‣ Currently, I'm working with
+                Typescript and Next.js predominantly. All my projects are open-sourced. You may
+                visit my Github profile to know more about my recent activities.
+              </div>
+            </div>
+            <div className="mb-4">
+              <div className="text-2xl tracking-tight font-black mb-3">UI/UX</div>
+              <div className="flex mb-2">
+                {/* <ArrowRightIcon w={2} h={2} mt={3} mr={4} /> */} ‣ I like building experiences
+                for the web. I build UI/UX concept designs and high-quality websites while looking
+                to power the web with my design aesthetics. If you want to check out some of my
+                work, you may visit my UI/UX portfolio @ boro-ui-ux.webflow.io.
+              </div>
+            </div>
+            <div className="mb-4">
+              <div className="text-2xl tracking-tight font-black mb-3">Technical Blogging</div>
+              <div className="flex mb-2">
+                {/* <ArrowRightIcon w={2} h={2} mt={3} mr={4} />  */}‣ I write technical blogs as
+                well. My blog circumvents around topics related to full-stack technologies,
+                features, updates, APIs, and development at large. You will find a lot of topics on
+                front-end technologies like React and Next. If you are interested, do have a visit
+                to the Blog section. Pretty sure you would find something readable.
+              </div>
+            </div>
+            <pre className="font-semibold">
+              &gt; Thats all me. Thanks for checking out.{' '}
+              <span role="img" aria-label="waving-emoji">
+                👋
+              </span>
+            </pre>
           </div>
         </div>
       </div>
