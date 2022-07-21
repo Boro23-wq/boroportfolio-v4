a0ee85d71efd9b6019e2e9b33a377fae2c21f89b
Sintu Boro <sintuboro@Sintus-MBP.fios-router.home>
2022-07-21 14:16:23 -0400

    modified item not found

--- pages/api/top-artists.js
+++ pages/api/top-artists.js
@@ -4,7 +4,11 @@ export default async (_, res) => {
   const response = await getTopArtists()
   const { items } = await response.json()
 
-  const artists = items.slice(0, 5).map((artist) => ({
+  if (!items) {
+    return res.status(404).json({ error: 'Data not found!' })
+  }
+
+  const artists = items?.slice(0, 5).map((artist) => ({
     name: artist.name,
     genres: artist.genres,
     totalFollowers: artist.followers.total,
