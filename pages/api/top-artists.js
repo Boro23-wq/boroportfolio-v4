bf99aa4183e124104aafc7c4e617dfa42196d316
Sintu Boro <sintuboro@Sintus-MBP.fios-router.home>
2022-07-21 14:24:32 -0400

    removed 400 status error

--- pages/api/top-artists.js
+++ pages/api/top-artists.js
@@ -4,10 +4,6 @@ export default async (_, res) => {
   const response = await getTopArtists()
   const { items } = await response.json()
 
-  if (!items) {
-    return res.status(404).json({ error: 'Data not found!' })
-  }
-
   const artists = items?.slice(0, 5).map((artist) => ({
     name: artist.name,
     genres: artist.genres,
