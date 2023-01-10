5741320c2516774ce01da1a4724fd744d27544ae
Sintu Boro <sintuboro@Sintus-MBP.lan>
2023-01-09 19:35:09 -0600

    updated about page

--- components/Track.js
+++ components/Track.js
@@ -3,7 +3,7 @@ import { truncate } from '@/lib/utils'
 export default function Track(track) {
   return (
     <div className="flex flex-row items-baseline border-b border-gray-200 dark:border-gray-700 max-w-3xl w-full mt-8 last:border-none">
-      <p className="text-sm font-bold text-gray-400 ">{track.ranking}</p>
+      <p className="text-sm font-bold text-gray-400">{track.ranking}</p>
       <div className="flex flex-col pl-3">
         <a
           className="font-medium text-gray-900 dark:text-gray-50 truncate w-60 sm:w-96 md:w-full"
@@ -11,10 +11,10 @@ export default function Track(track) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {truncate(track.title, 40)}
+          {truncate(track.title, 25)}
         </a>
         <p className="text-gray-500 mb-4 truncate w-60 sm:w-96 md:w-full" color="gray.500">
-          {truncate(track.artist, 40)}
+          {truncate(track.artist, 20)}
         </p>
       </div>
     </div>
--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,9 @@ import { PageSeo } from '@/components/SEO'
 import SpotifyPlayer from '@/components/SpotifyPlayer'
 import TopTracks from '@/components/TopTracks'
 import Image from 'next/image'
+import Link from 'next/link'
 import MediumPosts from '@/components/AllMediumPosts'
 import TopArtists from '@/components/TopArtists'
-import Analytics from '@/components/metrics/Analytics'
 
 export default function About() {
   return (
@@ -30,16 +30,16 @@ export default function About() {
               {siteMetadata.author}
             </h3>
             <div className="text-gray-500 dark:text-gray-400 mb-4">
-              Student, developer, and blogger.
+              Developer, Tech Advocate and Blogger.
             </div>
 
             <div className="flex justify-center sm:w-max mt-2">
               <SpotifyPlayer />
             </div>
 
-            <div className="w-full m-0 mt-4">
+            {/* <div className="w-full m-0 mt-4">
               <Analytics />
-            </div>
+            </div> */}
 
             <div className="w-full m-0 mt-4">
               <TopTracks />
@@ -54,38 +54,33 @@ export default function About() {
             {/*  about-section */}
             <div className="mb-8">
               <div className="text-3xl md:text-5xl text-black tracking-tight dark:text-white font-bold">
-                About
+                About Me
               </div>
 
               <div className="flex mb-2 px-2">
-                <ul className="list-[square]">
-                  <li>
-                    I'm a Computer Science graduate. I build high performant web applications. I
-                    have a design-centric approach, and love to devise high-impact user experiences.
-                  </li>
-                  <li>
-                    Currently, I'm working with Next.js, Nest.js, and GraphQL predominantly. All my
-                    projects are open-sourced. You may visit my Github profile to know more about my
-                    recent activities.
-                  </li>
-                  <li>
-                    Additionally, I like building experiences for the web. I build high-quality
-                    websites with design aesthetics. If you want to check out some of my work, you
-                    may visit my UI/UX portfolio @ boro-ui-ux.webflow.io.{' '}
-                  </li>
-                  <li>
-                    I write technical blogs as well. My blogs are mostly around topics related to
-                    full-stack development, features, updates, and APIs, at large. You will find a
-                    lot of topics on front-end technologies like React and Next.
-                  </li>
-                </ul>
+                <div className="flex flex-col mt-4">
+                  <div className="dark:bg-gray-800 border-2 dark:border-gray-700 rounded-md p-4 mb-3">
+                    ⦿ Graduated from New Jersey Institute of Technology with a Master's Degree in
+                    Computer Science, I am a self-motivated developer with 3+ years of experience
+                    eager to make a positive impact. I have previously built and delivered highly
+                    performant, design-centric web applications with excellent user experience.
+                  </div>
+                  <div className="dark:bg-gray-800 border-2 dark:border-gray-700 rounded-md p-4 mb-3">
+                    ⦿ Recently I have worked on a Care co-ordination and case management project
+                    where I led a team of 10+ members. My primary contribution was leading the API
+                    lifecycle development, building API services, and developing engaging UI/UX in
+                    an Agile environment.
+                  </div>
+                  <div className="dark:bg-gray-800 border-2 dark:border-gray-700 rounded-md p-4">
+                    ⦿ Additionally, I write technical blogs. My blogs are mostly around topics
+                    related to full-stack development, features, updates, and APIs, at large. You
+                    will find a lot of topics on front-end technologies like React and Next.
+                  </div>
+                </div>
               </div>
             </div>
 
-            <div className="m-0 sm:m-2">
-              <div className="mb-6 text-3xl md:text-5xl text-black tracking-tight dark:text-white font-bold">
-                Medium Stories
-              </div>
+            <div className="w-full m-0 mt-8">
               <MediumPosts />
             </div>
           </div>
